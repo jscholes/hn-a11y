26b45addc3fd87de4797c45c5bd5fa98f5b14c94
James Scholes <james@jamesscholes.com>
2016-10-10 00:08:57 +0100

    Remove the parentheses around domain names of submissions

--- hn-a11y.user.js
+++ hn-a11y.user.js
@@ -7,16 +7,28 @@
 // @license MIT
 // ==/UserScript==
 
+var NODE_TYPE_TEXT = 3;
 var POST_LINK_SELECTOR = 'a.storylink';
+var SITEBIT_SELECTOR = 'span.sitebit';
 
 var post_links = document.querySelectorAll(POST_LINK_SELECTOR);
-
 var post_link;
 for(post_link of post_links)
 {
-    // link.setAttribute('role', 'heading');
-    // link.setAttribute('aria-level', 2);
     var heading_node = document.createElement('h2');
     post_link.parentNode.replaceChild(heading_node, post_link);
     heading_node.appendChild(post_link);
+}
+
+var post_sites = document.querySelectorAll(SITEBIT_SELECTOR);
+var post_site;
+for(post_site of post_sites)
+{
+    for(child of post_site.childNodes)
+    {
+        if(child.nodeType == NODE_TYPE_TEXT)
+        {
+            child.remove();
+        }
+    }
 }
