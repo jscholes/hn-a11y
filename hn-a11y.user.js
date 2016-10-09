eb53eb24f7851365e4f0b670374bb222b6f92c6e
James Scholes <james@jamesscholes.com>
2016-10-09 23:44:54 +0100

    Add a heading level 2 to HN post titles

--- hn-a11y.user.js
+++ hn-a11y.user.js
@@ -7,3 +7,16 @@
 // @license MIT
 // ==/UserScript==
 
+var POST_LINK_SELECTOR = 'a.storylink';
+
+var post_links = document.querySelectorAll(POST_LINK_SELECTOR);
+
+var post_link;
+for(post_link of post_links)
+{
+    // link.setAttribute('role', 'heading');
+    // link.setAttribute('aria-level', 2);
+    var heading_node = document.createElement('h2');
+    post_link.parentNode.replaceChild(heading_node, post_link);
+    heading_node.appendChild(post_link);
+}
