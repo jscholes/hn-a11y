1ebf83a8d3171c3faaeb0c5687a55dda5e55b266
James Scholes <james@jamesscholes.com>
2016-10-09 22:16:56 +0100

    Add script metadata block

---
+++ hn-a11y.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name HN Accessibility
+// @description Screen reader accessibility improvements when navigating Hacker News
+// @namespace http://jscholes.net
+// @version 0.1
+// @include https://news.ycombinator.com/*
+// @license MIT
+// ==/UserScript==
+
